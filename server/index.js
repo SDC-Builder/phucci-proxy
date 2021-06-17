4786214645c8ab1e0c900afa266a6ce0056495d6
phucciTr <miabam26@gmail.com>
2021-06-17 00:24:36 -0700

    ssr with hydrate

--- server/index.js
+++ server/index.js
@@ -4,9 +4,16 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const axios = require('axios');
 require('newrelic');
+const fs = require('fs');
 
 const app = express();
 const PORT = 4000;
+
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import TitleService from './../phucci-titleBanner-service/client/src/template.jsx';
+
+console.log('TitleService = ', TitleService);
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
@@ -17,15 +24,23 @@ app.use(bodyParser.json());
 app.use(express.static(path.resolve(__dirname, '../public')));
 // app.use(morgan('dev'));
 
+const Title = ReactDOMServer.renderToString(<TitleService />);
+const indexHtmlDir = path.resolve('./public/index.html');
+let renderedHtml;
 
-app.get('/:id', (req, res) => {
-  return res.sendFile(path.resolve(__dirname, '../public', 'index.html'));
+fs.readFile(indexHtmlDir, 'utf8', (err, data) => {
+  if (err) { return console.log('ERROR READING HTML = ', err); }
+  renderedHtml = data.replace('<div id="title"></div>', `<div id="title">${Title}</div>`);
 });
 
-app.get('/api/title/:id', (req, res) => {
-  return res.redirect(`http://18.144.174.185/api/title/${req.params.id}`);
-
+app.get('/', (req, res) => {
+  return res.send(renderedHtml);
 });
+
+app.get('/:id', async (req, res) => {
+  return res.send(renderedHtml);
+});
+
 
 app.listen(PORT, () => {
   console.log(`Proxy listening at port ${PORT}`);
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+/* eslint-disable no-unused-vars */
+
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+require("@babel/polyfill"); // necesarry
+
+
+module.exports = {
+  mode: 'development',
+  entry: ['@babel/polyfill', path.join(__dirname, './server/index.js')],
+
+  target: 'node',
+
+  externals: [nodeExternals()],
+
+  output: {
+    filename: 'index.js',
+    path: path.resolve('server-build')
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js?/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        }
+      },
+
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      }
+
+    ]
+  },
+
+};
